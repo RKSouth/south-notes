5d628080bd288c2f8ed9d08fc915560a87e00c8e
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-01-13 13:21:24 -0800

    created User Model

---
+++ models/User.js
@@ -0,0 +1,13 @@
+// hold details about the schema -even though it is schema but gives us more safety when working with our server code
+
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+    username: String,
+    // will handle the require in graphql
+    password: String,
+    email: String,
+    createdAt: String
+});
+
+module.exports = model('User', userSchema);
