2e086199a974a617ad43f5d71434a33e141739f2
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-01-14 14:41:44 -0800

    crreated different md file for instructions

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const checkAuth = require('../../util/check-auth')
 const Post = require("../../models/Post")
@@ -30,6 +30,25 @@ module.exports = {
                     return post;
                 
             } else throw new UserInputError('Post not found');
+        },
+        async deleteComment(_, { postId, commentId}, context){
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if(post){
+                const commentIndex = post.comments.findIndex(c => c.id === commentId);
+
+                if(post.comments[commentIndex].username === username){
+                    post.comments.splice( commentIndex, 1);
+                    await post.save();
+                    return post;
+                } else {
+                    throw new AuthenticationError('Action not Allowed!!')
+                }
+            } else {
+                throw new UserInputError('Post not found')
+            }
         }
     }
 }
