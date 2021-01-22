98ba3b95995229d3e323305dc204c591fcb81fbf
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-01-22 13:24:40 -0800

    created PostForm component

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Form, Card, Icon, Label, Button, Image} from 'semantic-ui-react'
+
+import { useForm } from '../util/hooks'
+// import moment from 'moment'
+// import {Link} from "react-router-dom"
+
+function PostForm() {
+
+    return (
+   <Form>
+       <h2>Wooords</h2>
+   </Form>
+    )
+}
+
+export default PostForm
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Grid } from 'semantic-ui-react';
 import { useQuery } from "@apollo/react-hooks";
 import gql from 'graphql-tag';
 
+
+import { AuthContext} from '../context/auth'
 import PostCard from './../components/PostCard'
+import PostForm from './../components/PostForm'
+
 function Home() {
-    // const { loading, data: { getPosts: posts } } = useQuery(FETCH_POSTS_QUERY);
-    const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY);
+    // just signifying if user is signed
+    const { user } =useContext(AuthContext)
+
+    // show da posts!
+    const { 
+        loading, 
+        data: { getPosts: posts } = {} 
+    } = useQuery(FETCH_POSTS_QUERY);
     // if(data){
     //     console.log(data)
     // }
+
     return (
         <div>
         <div>
@@ -18,9 +29,14 @@ function Home() {
         <Grid.Row className="page-title">
             <h1>Recent Posts</h1>  
         </Grid.Row>
-     
         <Grid columns={3} >
     <Grid.Row>
+        {/* if user are logged in show this form */}
+        {user && (
+            <Grid.Column>
+                <PostForm/>
+            </Grid.Column>
+        )}
       {loading ? (
           <h1> Loading posts...</h1>
       ) : ( 
